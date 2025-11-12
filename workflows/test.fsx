3b58afcc6028ec9367731829248d55f44b4ba468
Pauline Hans <mrspaulinehans@gmail.com>
2025-11-12 17:24:53 +0100

    setting up test ARC for integrating a CQC to QualIQon

---
+++ workflows/test.fsx
@@ -0,0 +1,10 @@
+//test CQC for QualIQon - starting with getting accses of data stored in the ARC 
+
+#r "nuget: ARCtrl"
+
+open ARCtrl
+
+let arc = ARC()
+let arcPath = @"/home/paulinehans/Dokumente/TestARCForQualIQon"
+let loadingARC = ARC.load(arcPath)
+let writing = arc.Write(arcPath)
